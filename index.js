8d5d792daf358216e69a354203d02fbb969fb5bc
Terry Mitchelle <mitchellterry307@gmail.com>
2023-10-01 12:06:56 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2;
+const num2 =31;
+const multiply= num1*num2;
+
+const random= Math.floor(Math.random() * 10) + 1;
+
+const num3 = 22;
+const num4 = 6;
+const mod=4;
+num3%num4===4;
+
+
+const max= Math.max(1,20)
